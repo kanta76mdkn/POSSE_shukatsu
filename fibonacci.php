238174f94d0b42f236712b9fb55110fb4a04ab82
kanta76mdkn <kanta20021476@gmail.com>
2023-07-11 23:03:46 +0900

    なんか完成

--- fibonacci.php
+++ fibonacci.php
@@ -6,22 +6,19 @@ $count = 0;
 
 //コマンドで打った文字を取得する
 $num1 = intval(fgets(STDIN));
-$num1 = $argv[1];
-
 
 $index[0] = 0;
 $index[1] = 1;
-
-if($num1 ==0){
-    echo $index[0];
-} else if($num1 ==1){
-    echo $index[1];
-} else {
-    for($i = 2; $i <= $num1; $i++){
-        $index[$i] = $index[$i-1] + $index[$i-2];
-    }
-    echo $index[$num1];
-
+for($i = 2; $i <= $num1; $i++){
+    $index[$i] = $index[$i-1] + $index[$i-2];
 }
+echo $index[$num1-1];
+
+// if($num1 ==0){
+//     echo $index[0];
+// } else if($num1 ==1){
+//     echo $index[1];
+// } else {
+// }
 
 ?>
